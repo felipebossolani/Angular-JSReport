340e2d53193f4d9728dc3e758441b3dab92d420c
Felipe Bossolani <fbossolani@gmail.com>
2019-10-03 10:07:54 -0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as FileSaver from "file-saver";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'JSReport-Sample';
+
+  constructor(
+    private http: HttpClient,
+
+  ) {}
+
+
+  downloadFile(){
+    this.getFile().subscribe((res) => {
+      this.saveAsPDF(res, 'invoice');
+    }, (err) => {
+      console.error(err);
+    });
+  }
+
+  getFile() {
+    return this.http.get('https://localhost:5489/templates/HJH11D83ce', { responseType: 'blob' });
+  }
+
+  saveAsPDF(blob: Blob, fileName: string) {
+    let file = new Blob([blob], { type: 'application/pdf' });
+    FileSaver.saveAs(file, `${fileName}.pdf`);
+  }
 }
